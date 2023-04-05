288a233eb03ee4166429d6dd1bbed57a5aa2f3ed
Aluna-Andr <129425225+Aluna-Andr@users.noreply.github.com>
2023-04-05 12:48:34 +0000

    mais console log

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // o que node js - 
 //videos 4 e 5 da playlist,, os consideram os mains import
-  var x,y,soma;
-  x = 5; 
-  y='10';
-  soma=x+y;
-  console.log(soma);
+var x, y, soma;
+x = 5;
+y = '10';
+soma = x + y;
+console.log(soma);
+console.log(soma);
+console.log(soma);
